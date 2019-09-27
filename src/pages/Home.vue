a3eba94e2bfc6f11cddb31a5f3647186bc84e018
unknown <kelkarkulbhushan@gmail.com>
2019-09-27 22:39:29 +0530

    home2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,39 +1,30 @@
-<!--<template>
-     <div>
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <link href='https://fonts.googleapis.com/css?family=Clicker Script' rel='stylesheet'>
-      <link href="https://fonts.googleapis.com/css?family=Notable&display=swap" rel="stylesheet">
-       <img src="https://images.pexels.com/photos/908284/pexels-photo-908284.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" width="1500" height="656">
-<div style="position:fixed;height:60px;width:100%;top:0px;left:0;background-color: black; z-index:1">
-     <h4 style="display: inline-block; margin-top:15px; margin-left:20px; color:white;font-family: 'Clicker Script'; font-size=30px">Codification</h4>
-        </div>
-        <div>
-         <q-btn color="amber" glossy label="Amber" />
-          </div> 
-     
-          
-    </div>
-    
-  </template>-->
  <template>
 <div>
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href='https://fonts.googleapis.com/css?family=Clicker Script' rel='stylesheet'>
+   <img src="https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940s" width="1500" height="656">
    <div style="position:fixed;height:60px;width:100%;top:0px;left:0;background-color: black; z-index:1">
      <h4 style="display: inline-block; margin-top:15px; margin-left:550px; color:white;font-family: 'Clicker Script'; font-size=30px">Codification</h4>
-      </div> 
-    <q-page class="flex flex-center">
-    <img src="https://images.unsplash.com/photo-1533709752211-118fcaf03312?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" width="1500" height="656">
-  
-  </q-page>
+  </div>
+   <div class="q-pa-md row items-start q-gutter-md">
+     <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6"> STEP UP YOUR CODING GAME</div>
+         </q-card-section> 
+         <q-card-section>
+     The new way to improve your programming skills while having fun and getting noticed
+      </q-card-section>
+    </q-card>
+     </div>
+    
+     
    
 <div class="fixed-center" style=" display:block; margin:auto; padding:500px 0 0 0;" >
-    <!-- <div style="bottom:10px"> -->
-        <q-btn label="Get Started"  color="amber"/>
-    
-   
-     
-   </div>
+   <q-btn label="Get Started"  color="amber"/>
+    </div>
  </div>
 
    </template>
@@ -58,6 +49,11 @@
      }
    }
    </script>
+   <style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
      <style>
      
      body{
